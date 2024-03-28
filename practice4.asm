ce8a1d080e1a2b81639d926f99704d4927f9fdd5
LizaKlymenko <elis.klymenko@gmail.com>
2024-03-29 00:12:15 +0200

    Читання та обробка введених символів. Додані файли тестування

--- practice4.asm
+++ practice4.asm
@@ -1,60 +1,37 @@
 .model small
 .stack 100h
-
+.386
 .data
-    srcMsg db "1234567890123456$", 0   ; Десяткове число в рядковому форматі
-    destMsg db 32 dup(?)               ; Місце для завдання 2: суми 16-бітних чисел
-
+    oneChar db ?, '$'
+    array dw 10000 Dup(?)
+    size_of_array dw ?
+    
+    
 .code
+start:
+    ; Иниціализація сегмента данных
     mov ax, @data
     mov ds, ax
 
-    ; Завдання 1: конвертує десяткове значення у бінарне представлення (слово) у доповнювальному коді
-    mov si, offset srcMsg
-    mov di, offset destMsg
-    call decimalToBinaryComplement
+    xor ax, ax
+    xor di, di
+    xor si, si
+    mov array[0], 0
+    xor si, si
+    read_next:
+        mov ah, 3Fh
+        mov bx, 0h  ; ввід даних з stdin 
+        mov cx, 1   ; 1 byte to read
+        mov dx, offset oneChar   ;
+        int 21h          ;Виклик DOS-служби для чит символу
 
-    ; Завдання 2: сумує 16-бітні числа та накопичує результат у 32-бітному (2 слова)
-    call sum16bitNumbers
+        or ax, ax;  ax = number of bytes read
+        je stop_reading
 
-    mov ah, 4Ch
-    int 21h
+        cmp oneChar, ' '; Перевірка, чи символ - пробіл
+        jz add_number_to_array
 
-decimalToBinaryComplement PROC
-    mov cx, 16             ; Лічильник для циклу
-
-    cld                    ; Очистити прапор напрямку (читати зліва направо)
-
-convertLoop:
-    mov al, [si]           ; Завантажити поточний символ в al
-    cmp al, '$'            ; Перевірити, чи досягнуто кінця рядка
-    je endConversion       ; Якщо символ '$', завершити конвертацію
-    sub al, '0'            ; Конвертувати ASCII від '0' до числа
-    not al                 ; Інвертувати біти у числі
-    stosb                  ; Записати байт в destMsg
-    inc si                 ; Перейти до наступного символу
-    loop convertLoop
-
-endConversion:
-    ret
-decimalToBinaryComplement ENDP
-
-sum16bitNumbers PROC
-    mov cx, 8
-
-    mov si, offset srcMsg  ; Адреса першого числа
-    mov di, offset destMsg ; Адреса для накопичення результату
-
-sumLoop:
-    mov ax, 0             ; Обнулити регістр ax
-    lodsw                 ; Завантажити 16-бітне число в ax
-    add word ptr [di], ax ; Додати 16-бітне число до результату в destMsg
-    adc word ptr [di+2], 0 ; Додати carry до старших біть
-
-    add di, 4             ; Перейти до наступного 16-бітного числа у destMsg
-    loop sumLoop
-
-    ret
-sum16bitNumbers ENDP
-
-end
+        cmp oneChar, 13 ; перевірка чи це CR
+        je add_number_to_array
+        cmp oneChar, 10 ; LF
+        je add_number_to_array
